0ab1d694b7142ee9771a85f7f07c94026cfb7521
SumanthDusa <sumanthdusa939@gmail.com>
2026-05-13 09:02:20 +0530

    Update ContactsPage.jsx

--- frontend/src/pages/ContactsPage.jsx
+++ frontend/src/pages/ContactsPage.jsx
@@ -31,7 +31,7 @@ export function ContactsPage() {
 
       const response =
         await axios.get(
-          "${import.meta.env.VITE_API_URL}/api/contacts",
+          '${import.meta.env.VITE_API_URL}/api/contacts',
           {
             headers: {
               Authorization: `Bearer ${token}`,
