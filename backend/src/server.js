154fbfd2c55cb15c6f38699ca24346f0dcda74cc
SumanthDusa <sumanthdusa939@gmail.com>
2026-05-13 08:04:04 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/contacts", contactsRoutes);
 
