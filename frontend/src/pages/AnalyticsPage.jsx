1c82f5e855e9102ada5525809c0d15e0579858f9
SumanthDusa <sumanthdusa939@gmail.com>
2026-05-13 08:48:02 +0530

    Update AnalyticsPage.jsx

--- frontend/src/pages/AnalyticsPage.jsx
+++ frontend/src/pages/AnalyticsPage.jsx
@@ -38,7 +38,7 @@ export function AnalyticsPage() {
 
       const response =
         await axios.get(
-          "http://localhost:5000/api/analytics",
+          "${import.meta.env.VITE_API_URL}/api/analytics",
           {
             headers: {
               Authorization: `Bearer ${token}`,
