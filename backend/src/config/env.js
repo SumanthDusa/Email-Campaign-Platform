e00fa886768c9bcd3403c16eb13e1dc2b5a2faae
SumanthDusa <sumanthdusa939@gmail.com>
2026-05-13 11:05:46 +0530

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,8 +5,8 @@ dotenv.config();
 const env = {
   port: Number(process.env.PORT || 5000),
   jwtSecret: process.env.JWT_SECRET || "change-me",
-  smtpEmail: process.env.SMTP_EMAIL || "",
-  smtpPassword: process.env.SMTP_PASSWORD || "",
+  smtpEmail: process.env.EMAIL_USER || "",
+  smtpPassword: process.env.EMAIL_PASS || "",
   emailBatchSize: Number(process.env.EMAIL_BATCH_SIZE || 25),
 };
 
