4f23b2822d3ecd4bbec4a4716d59fd4d1884bba6
SumanthDusa <sumanthdusa939@gmail.com>
2026-05-13 08:50:07 +0530

    Update TemplatesPage.jsx

--- frontend/src/pages/TemplatesPage.jsx
+++ frontend/src/pages/TemplatesPage.jsx
@@ -31,7 +31,7 @@ export  function TemplatesPage() {
 
       const response =
         await axios.get(
-          "http://localhost:5000/api/templates",
+          "${import.meta.env.VITE_API_URL}/api/templates",
           {
             headers: {
               Authorization: `Bearer ${token}`,
