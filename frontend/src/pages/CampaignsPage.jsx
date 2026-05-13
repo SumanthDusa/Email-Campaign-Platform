ec4dd759e04223edfeb37e4d86861bafba556bbd
SumanthDusa <sumanthdusa939@gmail.com>
2026-05-13 08:49:25 +0530

    Update CampaignsPage.jsx

--- frontend/src/pages/CampaignsPage.jsx
+++ frontend/src/pages/CampaignsPage.jsx
@@ -45,7 +45,7 @@ export function CampaignsPage() {
 
       const response =
         await axios.get(
-          "http://localhost:5000/api/campaigns",
+          "${import.meta.env.VITE_API_URL}/api/campaigns",
           {
             headers: {
               Authorization: `Bearer ${token}`,
